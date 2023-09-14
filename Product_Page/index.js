d834f80dce6a7d6f5a4183e2ccadc8b3b5495fc8
KGS97 <work.dipanshu.singh@gmail.com>
2023-09-14 20:07:59 +0530

    filters complete

--- Product_Page/index.js
+++ Product_Page/index.js
@@ -7,46 +7,53 @@ let houseplants = false, houseplant_sets = false,flowerpots=false,soil_fertilize
 let houseplantFilter = document.getElementById('houseplant_filter')
 houseplantFilter.onchange=()=>{
     houseplants=!houseplants
-    filter()
+    render(sortNfilter())
 }
 let priceRangeFilter = document.getElementById('price_range_filter')
 priceRangeFilter.onchange = ()=>{
-    let val = event.target.value;
-    render(products.filter(el=>parseInt(el.price)<=val))
-}
-let filter = () => {
-    render(products.filter(el=>{
-        if(!houseplants && !houseplant_sets && !flowerpots && !soil_fertilizers)return true;
-        let res = false;
-        if(houseplants)res=res||el.category==="Houseplants";
-        if(houseplant_sets)res=res||el.category==="Houseplant Sets"
-        if(flowerpots)res=res||el.category==="Flowerpot"
-        if(soil_fertilizers)res= res || el.category==="Soil" || el.category==="Fertilizer";
-        return res;
-    }))
+    render(sortNfilter())
 }
 let houseplantSetsFilter = document.getElementById('houseplant_set_filter')
 houseplantSetsFilter.onchange=()=>{
- houseplant_sets=!houseplant_sets
-    filter()   
+    houseplant_sets=!houseplant_sets
+    render(sortNfilter())   
 }
 let flowerpotFilter = document.getElementById('flowerpot_filter')
 flowerpotFilter.onchange=()=>{
     flowerpots=!flowerpots
-    filter()
+    render(sortNfilter())
 }
 let soilFertilizerFilter = document.getElementById('soil_and_fertilizers_filter')
 soilFertilizerFilter.onchange=()=>{
     soil_fertilizers=!soil_fertilizers
-    filter()
+    render(sortNfilter())
 }
 let sorting =document.getElementById('sorting')
-sorting.onchange=()=>{
-    let val = event.target.value
-    let temp = [...products]
-    if(val==="atoz")render(temp.sort((a,b)=>a.name.localeCompare(b.name)))
-    else if(val==="ztoa")render(temp.sort((a,b)=> -a.name.localeCompare(b.name)))
-    else if(val==="lowtohigh")render(temp.sort((a,b)=> a.price-b.price))
-    else if(val==="hightolow")render(temp.sort((a,b)=> b.price-a.price))
-    else render(products)
+sorting.onchange=()=>render(sortNfilter())
+let sortNfilter = () => {
+    let filtered = productFilter()
+    let sortedNFiltered = productSort(filtered)
+    return sortedNFiltered;
+}
+let productFilter = () =>{
+    let res = products.filter(el=>{
+        if(!houseplants && !houseplant_sets && !flowerpots && !soil_fertilizers)return el.price<=priceRangeFilter.value;
+        let res = false;
+        if(houseplants)res = res || el.category === "Houseplants";
+        if(houseplant_sets)res = res || el.category === "Houseplant Sets"
+        if(flowerpots)res = res || el.category === "Flowerpot"
+        if(soil_fertilizers)res = res || el.category === "Soil" || el.category === "Fertilizer";
+        return el.price<=parseInt(priceRangeFilter.value) && res;
+    })
+    console.log(res)
+    return res;
+}
+let productSort=(data)=>{
+    let val = sorting.value
+    let temp = [...data]
+    if(val==="atoz")return temp.sort((a,b)=> a.name.localeCompare(b.name))
+    else if(val==="ztoa")return temp.sort((a,b)=> -a.name.localeCompare(b.name))
+    else if(val==="lowtohigh")return temp.sort((a,b)=> a.price-b.price)
+    else if(val==="hightolow")return temp.sort((a,b)=> b.price-a.price)
+    else return temp
 }
