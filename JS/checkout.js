c3ac8a3a2bf6f1697b6979b5974808c30c0ee366
kumod1164 <kumodsharma1164@gmail.com>
2023-09-14 13:11:54 +0530

    Cart Skeleton

--- JS/checkout.js
+++ JS/checkout.js
@@ -1,17 +1,22 @@
 var options = {
     key: 'rzp_test_PuIvwrP2D7FLip', 
-    amount: 100*50000, // Amount in paise (100 paise = 1 INR)
+    amount: 100 * 50000, // Amount in paise (100 paise = 1 INR)
     currency: 'INR',
-    name: 'Your Company Name',
+    name: 'Green Paradise',
     description: 'Payment for your order',
-    image: 'your_logo_url.png', 
+    image: '../images/logo.png',
    
     handler: function(response) {
-        
-        alert('Payment successful: ' + response.razorpay_payment_id);
+        if (response.razorpay_payment_id) {
+           
+            window.location.href = 'your_homepage_url.html'; 
+        } else {
+           
+            alert('Payment failed or canceled.');
+        }
     },
     prefill: {
-        name: 'John Doe',
+        name: '',
         email: 'john@example.com',
         contact: '9876543210'
     }
@@ -19,7 +24,7 @@ var options = {
 
 var rzp = new Razorpay(options);
 
-document.getElementById('rzp-button').addEventListener('click', function(e) {
+document.getElementById('kkpaybtn').addEventListener('click', function(e) {
     rzp.open();
     e.preventDefault();
 });
